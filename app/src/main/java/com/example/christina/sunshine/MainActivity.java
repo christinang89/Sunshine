5db321960970b86e71a74bf1857153a1f0f91c67
Christina Ng <christinang89@gmail.com>
2014-10-06 22:54:07 -0700

    added settings activity

--- app/src/main/java/com/example/christina/sunshine/MainActivity.java
+++ app/src/main/java/com/example/christina/sunshine/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.christina.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -34,6 +35,7 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
